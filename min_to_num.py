c668ef12c57a85796d7b12d973bf958de4b6bfd8
am7ir <amho.jahan@gmail.com>
2025-12-04 22:09:25 +0330

    Fist Commit

---
+++ min_to_num.py
@@ -0,0 +1,21 @@
+'''
+This program compute your legal age
+Amir Jahandideh
+'''
+
+def get_min(x,y):
+    '''
+    Docstring for get_min
+    این تایع بین دو عدد کوپکترین را پیدا می کند و بر می گرداند
+    :param x: عدد اول
+    :param y: عدد دوم
+    '''
+    mn = x
+    if y< x:
+        mn = y
+    return y
+
+a = float(input("num 1 : " ))
+b = float(input("num 2 : "))
+mn = get_min(a,b)
+print (mn)
